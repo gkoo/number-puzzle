64f566579cc760cfa6447792dd6a5774a97274a7
Gordon Koo <gordon@opendoor.com>
2018-06-24 21:49:11 -0700

    fill perfect fit

--- puzzle.rb
+++ puzzle.rb
@@ -2,7 +2,6 @@
 
 class Cell
   attr_reader :x, :y, :filled
-  attr_accessor :completed
 
   def initialize(x, y)
     @x = x
@@ -12,11 +11,16 @@ class Cell
   end
 
   def completed?
-    completed
+    @completed
   end
 
   def fill_in
     @filled = true
+    @completed = true
+  end
+
+  def set_blank
+    @filled = false
     @completed = true
   end
 end
@@ -64,26 +68,29 @@ class Puzzle
     # process columns
     grid_width.times do |col_number|
       column_cells = get_cell_list_by_column(col_number)
-      clue_group = col_clues[col_number]
-      process_cell_list(column_cells, clue_group)
+      clue_list = col_clues[col_number]
+      do_processing(column_cells, clue_list)
     end
 
     grid_height.times do |row_number|
       row_cells = get_cell_list_by_row(row_number)
-      clue_group = row_clues[row_number]
-      process_cell_list(row_cells, clue_group)
+      clue_list = row_clues[row_number]
+      do_processing(row_cells, clue_list)
     end
   end
 
   private
 
-  def process_cell_list(cell_list, cell_list_clues)
+  def do_processing(cell_list, clue_list)
+    process_cell_list(cell_list, clue_list)
+    fill_perfect_fit(cell_list, clue_list)
+  end
+
+  def process_cell_list(cell_list, clue_list)
     return if cell_list.all? { |cell| cell.completed? }
 
-    # if there's only one clue in the clue group which indicates that the majority of the cells in
-    # a row or column are filled, then fill in the middle.
-    if cell_list_clues.length == 1
-      clue = cell_list_clues.first
+    if clue_list.length == 1
+      clue = clue_list.first
       if clue > (cell_list.length / 2)
         fill_majority_middle(cell_list, clue)
       end
@@ -91,12 +98,31 @@ class Puzzle
   end
 
   def fill_majority_middle(cell_list, majority_size)
+    # if there's only one clue in the clue group which indicates that the majority of the cells in
+    # a row or column are filled, then fill in the middle.
     list_length = cell_list.length
     num_cells_to_fill_in = ((majority_size / 2 * 2) - (list_length / 2)) * 2
     to_leave_blank_size = (list_length - num_cells_to_fill_in) / 2
     start_idx = to_leave_blank_size
     end_idx = to_leave_blank_size + num_cells_to_fill_in - 1
     cell_list[start_idx..end_idx].each { |cell| cell.fill_in }
+  end
+
+  def fill_perfect_fit(cell_list, clue_list)
+    # if exactly one space goes between each clue, it's a perfect fit
+    return unless clue_list.reduce(:+) + clue_list.length - 1 == cell_list.length
+    idx = 0
+    clue_list.each do |clue|
+      clue.times do |i|
+        cell_list[idx].fill_in
+        idx += 1
+      end
+
+      if idx < cell_list.length
+        cell_list[idx].set_blank
+        idx += 1
+      end
+    end
   end
 
   def get_cell_list_by_column(col_num)
